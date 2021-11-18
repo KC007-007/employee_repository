a8cdb2bde464836c755ae8344b99698ce0e01fd9
KC007-007 <krishnayadav30894@gmail.com>
2021-11-18 09:57:58 +0530

    employee projects uploaded

---
+++ employee/src/main/java/com/drucare/employee/UpdateEmployeeBean.java
@@ -0,0 +1,85 @@
+package com.drucare.employee;
+
+public class UpdateEmployeeBean {
+
+	Integer empId;
+	String firstNm;
+	String middleNm;
+	String lastNm;
+	String mobileNo;
+	String email;
+	String addressLine1;
+	String addressLine2;
+
+	public Integer getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+
+	public String getFirstNm() {
+		return firstNm;
+	}
+
+	public void setFirstNm(String firstNm) {
+		this.firstNm = firstNm;
+	}
+
+	public String getMiddleNm() {
+		return middleNm;
+	}
+
+	public void setMiddleNm(String middleNm) {
+		this.middleNm = middleNm;
+	}
+
+	public String getLastNm() {
+		return lastNm;
+	}
+
+	public void setLastNm(String lastNm) {
+		this.lastNm = lastNm;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddressLine1() {
+		return addressLine1;
+	}
+
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
+
+	public String getAddressLine2() {
+		return addressLine2;
+	}
+
+	public void setAddressLine2(String addressLine2) {
+		this.addressLine2 = addressLine2;
+	}
+
+	@Override
+	public String toString() {
+		return "UpdateEmployeeBean [empId=" + empId + ",firstNm=" + firstNm + ", middleNm=" + middleNm + ", lastNm="
+				+ lastNm + ", mobileNo=" + mobileNo + ", email=" + email + ", addressLine1=" + addressLine1
+				+ ", addressLine2=" + addressLine2 + "]";
+	}
+
+}
